9f97926de493c6ef51416e4a6b82e88cabc33735
Dan Cartier <nospamdan1@gmail.com>
2018-02-24 12:33:04 -0500

    bacon: Bring back audio.conf

--- bacon.mk
+++ bacon.mk
@@ -20,6 +20,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml
